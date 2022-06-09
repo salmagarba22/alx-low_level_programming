e18ceaf7cc2689366b0f0acf38e5d2ebdbeabef6
Salamatu Adebanjo <salmagarba22@gmail.com>
2022-06-09 10:13:16 -0700

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,15 @@
 #include "main.h"
-#
-#/**
-# * mul- Function prototype
-#  * Description: Takes two integers as arguments and returns their product.
-#   * @a: The first number to multiply
-#    * @b: The second number to multiply
-#     * Return: The product of a and b
-#      */
-#      int mul(int a, int b)
-#      {
-#      	int p = a * b;
-#
-#      		return (p);
-#      		}
+
+/**
+* mul- Function prototype
+* Description: Takes two integers as arguments and returns their product.
+* @a: The first number to multiply
+* @b: The second number to multiply
+* Return: The product of a and b
+*/
+	int mul(int a, int b)
+{
+	int p = a * b;
+
+	return (p);
+}
