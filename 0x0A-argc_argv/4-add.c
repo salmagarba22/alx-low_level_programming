e507116cf85251df681a5cf6d0f96c2b3ad50c73
Salamatu Adebanjo <salmagarba22@gmail.com>
2022-06-24 11:58:01 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <type.h>
 #include "main.h"
 
 /**
  * main - Entry point
  * @argc: Number of arguments
  * @argv: Array of arguments
- *
  * Return: 0 on sucess or 1 if unsuccessful
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1)
-		printf("%d\n", 0);
+	if (argc < 2)
+	printf("0\n");
 	else
 	{
+		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]))
-				sum += atoi(argv[i]);
-			else
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
 
+			sum += atoi(argv[i]);
 		}
+
 		printf("%d\n", sum);
 	}
-
 	return (0);
 }
